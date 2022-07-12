77cf530ec73e5888abf2fb7fcaf23d77a6ebb993
Denis <demaz@bk.ru>
2022-07-12 15:40:52 +0700

    task 1 complited

--- src/homework_2.java
+++ src/homework_2.java
@@ -1,9 +1,17 @@
 public class homework_2 {
     public static void main(String[] args) {
+        // Task 1 --------------------
+        byte a = 1;
+        short b = 2000;
+        int c = 30000000;
+        long d = 4000L;
+        float e = 0.5f;
+        double f = 0.666;
+        char g = 'g';
+        boolean h = true;
+        //----------------------------
+
+        // Task 2 --------------------
 
     }
-
-
-
-
 }
